f02a1e90e2ffa6d0052507f95766fa166cbae5f3
Ayaan <desktop.ux@gmail.com>
2026-02-08 04:21:16 +0530

    link backend added

--- src/api/api.jsx
+++ src/api/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://socio-app-backend-yfma.onrender.com/api",
+  baseURL:  import.meta.env.VITE_API_BASE_URL,
   withCredentials: true 
 });
 
