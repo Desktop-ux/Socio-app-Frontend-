78e078f97b79af46410426464287693a7235c9cf
Ayaan <desktop.ux@gmail.com>
2026-02-08 21:53:51 +0530

    token error fix

--- src/api/api.jsx
+++ src/api/api.jsx
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: "https://socio-app-backend-yfma.onrender.com/api",
+  withCredentials: true,
 });
 
 api.interceptors.request.use(
